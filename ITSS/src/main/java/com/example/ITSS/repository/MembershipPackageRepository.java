6247351695c4ed8948e1780c988b11b10d65d732
Vinh Nguyen <vinh12112004@gmail.com>
2025-06-11 18:27:48 +0700

    package

---
+++ ITSS/src/main/java/com/example/ITSS/repository/MembershipPackageRepository.java
@@ -0,0 +1,10 @@
+package com.example.ITSS.repository;
+
+import com.example.ITSS.model.MembershipPackage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface MembershipPackageRepository extends JpaRepository<MembershipPackage, Long> {
+    List<MembershipPackage> findByMemberId(Long memberId);
+}
